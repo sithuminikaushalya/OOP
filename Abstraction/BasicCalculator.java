dc879861b7bdf04451853d42c3de4afa33bc42e7
Sithumini Kaushalya <118107125+sithuminikaushalya@users.noreply.github.com>
2024-06-05 18:42:15 +0530

    first commit

---
+++ Abstraction/BasicCalculator.java
@@ -0,0 +1,34 @@
+package Abstraction;
+
+// BasicCalculator class that extends the abstract class Calculator
+public class BasicCalculator extends Calculator {
+    // Override method to add a value to the result
+    @Override
+    public void add(int value) {
+        result += value;
+    }
+
+    // Override method to subtract a value from the result
+    @Override
+    public void subtract(int value) {
+        result -= value;
+    }
+
+    // Main method to test the BasicCalculator class
+    public static void main(String[] args) {
+        // Create an instance of BasicCalculator
+        BasicCalculator calc = new BasicCalculator();
+
+        // Test add method
+        calc.add(10);
+        System.out.println("Result after adding 10: " + calc.getResult());
+
+        // Test subtract method
+        calc.subtract(5);
+        System.out.println("Result after subtracting 5: " + calc.getResult());
+
+        // Test reset method
+        calc.reset();
+        System.out.println("Result after reset: " + calc.getResult());
+    }
+}
---
+++ Abstraction/Calculator.java
@@ -0,0 +1,24 @@
+package Abstraction;
+
+// Abstract class Calculator that contains the structure for a basic calculator
+public abstract class Calculator {
+    // Protected variable to store the result
+    protected int result;
+
+    // Abstract method to add a value to the result
+    public abstract void add(int value);
+
+    // Abstract method to subtract a value from the result
+    public abstract void subtract(int value);
+
+    // Method to get the current result
+    public int getResult() {
+        return result;
+    }
+
+    // Method to reset the result to 0
+    public void reset() {
+        result = 0;
+    }
+}
+
